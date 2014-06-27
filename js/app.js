b346d60221e42ff973453f752f7bd02d949d45df
Khemraj Thapa <khyamay@gmail.com>
2014-06-27 23:49:42 +0100

    Added TweenMax animation

--- js/app.js
+++ js/app.js
@@ -26,5 +26,27 @@ angular.module('slider', ['ngAnimate'])
 			$scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
 
 		};
-		
+
+	})
+	.animation('.slide-animation', function (){
+		return {
+			addClass: function (element, className, done){
+				if (className == 'ng-hide'){
+					TweenMax.to(element, 0.5, { left: -element.parent().width(), onComplete: done});
+				} else {
+					done();
+				}
+			},
+
+			removeClass: function (element, className, done){
+				if (className == 'ng-hide'){
+					element.removeClass('ng-hide');
+
+					TweenMax.set(element, { left: element.parent().width()});
+					TweenMax.to(element, 0.5, {left: 0, onComplete: done});
+				} else {
+					done();
+				}
+			}
+		};
 	});
